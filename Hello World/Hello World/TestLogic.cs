cfcdf8eefd0a3e339fa92fad327306f04ea064a5
pmccusker <patrick.mccusker@energia.ie>
2016-11-04 11:23:34 +0000

    Unit tests

---
+++ Hello World/Hello World/TestLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    public class TestLogic
+    {
+        public int go(int iCount)
+        {
+            int iRet = 0;
+            for (int i = 0; i < iCount; i++)
+            {
+                iRet += i;
+            }
+
+            return iRet;
+        }
+    }
+}
---
+++ Hello World/HelloTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Hello_World.TestLogic oLogic = new Hello_World.TestLogic();
+
+
+            Assert.AreEqual(oLogic.go(0), 0);
+            Assert.AreEqual(oLogic.go(1), 0);
+            Assert.AreEqual(oLogic.go(2), 1);
+
+
+        }
+    }
+}
