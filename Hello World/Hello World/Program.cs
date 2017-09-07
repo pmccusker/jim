b537bece2d881cd3571411767c303ca0cf6a97a6
pmccusker <patrick.mccusker@energia.ie>
2017-09-07 13:45:48 +0100

    Update Program.cs

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -11,7 +11,7 @@ namespace Hello_World
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        // New comment
+        // New comment    
         [STAThread]
         static void Main()
         {
